289317d3e7fde1d94998333754d65dd00d55905b
sri suriya <155533834+srisuriya13022006@users.noreply.github.com>
2024-05-22 10:18:01 +0530

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,1 +1,9 @@
 #write a program to find the factorial of a nummber
+def factorial(num):
+    if num == 0 or num == 1:
+        return 1
+    else:
+        return num * factorial(num - 1)
+
+print(factorial(3))
+    
